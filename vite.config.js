2d83dfd50e9d101016374b59dba87b202d9a8135
ShivamDYSON <shivamprakash2004@gmail.com>
2024-08-30 05:51:43 -0700

    v

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // base: '/luxury-studio/', // This should match your repo name
+  base: '', // This should match your repo name
   plugins: [react()],
 });
